4da853eec1bc2bb40354e88ccf996d6889212429
DAVID T K LEUNG (DEV-ISD-OOCLL/HKG) <LEUNGDA3@OOCL.COM>
2020-11-26 17:02:36 +0800

    [MarsRover][David Leung]test: add Move from 0 0 S case

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -8,14 +8,24 @@ class MarsRoverTest {
     public void should_return_y_plus_1_when_executeCommands_given_0_0_N_and_M() {
         //given
         MarsRover marsRover = new MarsRover(0,0,"N");
-
         //when
         marsRover.executeCommands("M");
-        
         //then
         assertEquals(0,marsRover.getLocationX());
         assertEquals(1,marsRover.getLocationY());
         assertEquals("N",marsRover.getDirection());
+    }
+
+    @Test
+    public void should_return_y_minus_1_when_executeCommands_given_0_0_S_and_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"S");
+        //when
+        marsRover.executeCommands("M");
+        //then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(-1,marsRover.getLocationY());
+        assertEquals("S",marsRover.getDirection());
     }
 
 }
